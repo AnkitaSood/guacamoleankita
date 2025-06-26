a01870e83e6f643a0bfefac34eb3ead5db84186e
ankitasood <asood@octolabs.io>
2025-06-25 22:55:46 -0500

    feat: update intro

--- src/app/pages/blog/index.page.ts
+++ src/app/pages/blog/index.page.ts
@@ -11,15 +11,14 @@ import PostAttributes from '../../post-attributes';
   template: `
     <h1>Hey there!</h1>
     <h2>My name is Ankita Sood</h2>
-    <h3>I am currently adulting in Austin, Texas - and still learning how to survive the aggressive heat
-      and increasing rent prices. Depending on the weather, I can be found outdoors hiking, biking, paddle boarding,
+    <h3>I'm a Google Developer Expert in Angular, currently based in Austin, Texas . Depending on the weather, I can be found outdoors hiking, biking, paddle boarding,
       trying to pet other peoples' dogs OR staying indoors reading a book. I have an avid interest in all things Design
       and Angular. I have been a frontend engineer all my adult working life, which is approximately either 10 years or
       100 years, because I have lost sense of time since the pandemic. </h3>
 
 
     @for (post of posts; track post.attributes.slug) {
-    <a [routerLink]="['/blog/', post.attributes.slug]">
+    <a [routerLink]="['/', post.attributes.slug]">
       <h2 class="post__title">{{ post.attributes.title }}</h2>
       <p class="post__desc">{{ post.attributes.description }}</p>
     </a>
