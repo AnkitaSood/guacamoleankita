af6c30985d8474e2276b944a697d3b61c360366b
ankitasood <asood@octolabs.io>
2025-06-26 00:45:35 -0500

    style: theming, style and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
     template: `
-    <router-outlet />
+    <header class="header">
+      <nav class="nav">
+        <div class="nav-container">
+          <div class="nav-brand">
+            <a [routerLink]="'/'" class="brand-link">Ankita Sood</a>
+          </div>
+          <ul class="nav-menu">
+            <li class="nav-item">
+              <a [routerLink]="'/'" class="nav-link" routerLinkActive="active">About Me</a>
+            </li>
+            <li class="nav-item">
+              <a [routerLink]="'/talks'" class="nav-link" routerLinkActive="active">Talks</a>
+            </li>
+            <li class="nav-item">
+              <a [routerLink]="'/blog'" class="nav-link" routerLinkActive="active">Posts</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main class="main-content">
+      <router-outlet />
+    </main>
   `,
     styles: ``
 })
